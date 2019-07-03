9f49c49a1096081cc85db6a7f5762b42daa8fd6c
Matheus Filipe <matheus_filipe00@outlook.com>
2019-07-02 23:44:32 -0300

    Fazendo os primeiros testes de movimentacao

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -18,5 +18,10 @@ namespace tabuleiro
             this.cor = cor;
             this.qteMovimentos = 0;
         }
+
+        public void incrementarQteMovimentos()
+        {
+            qteMovimentos++;
+        }
     }
 }
